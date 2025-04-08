b9d67be021373aa51989aca965407fd2b346906d
HNP2135 <hpatel5575@conestogac.on.ca>
2025-04-08 11:26:54 -0400

    Remove node_modules and lockfile to fix bcrypt issue

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 router.post('/manager', async (req, res) => {
   try {
